bc011e90d67479aee46c0d018117a8b0294fe61c
yeahupsrl <mainrobert53@gmail.com>
2025-08-09 08:47:08 +0200

    2 ads12

--- components/calculator/ProfessionalCalculatorLayout.tsx
+++ components/calculator/ProfessionalCalculatorLayout.tsx
@@ -334,25 +334,6 @@ export default function ProfessionalCalculatorLayout({
             )}
           </div>
 
-             {/* --- AdSense block --- sidebar*/}22222
-                <Script
-                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9476637732224939"
-                  strategy="afterInteractive"
-                  crossOrigin="anonymous"
-                />
-                <ins
-                  className="adsbygoogle"
-                  style={{ display: 'block' }}
-                  data-ad-client="ca-pub-9476637732224939"
-                  data-ad-slot="6880767372"
-                  data-ad-format="auto"
-                  data-full-width-responsive="true"
-                />
-                <Script id="ads-init" strategy="afterInteractive">
-                  {`(adsbygoogle = window.adsbygoogle || []).push({});`}
-                </Script>
-
-
         </div>
       </div>
 
